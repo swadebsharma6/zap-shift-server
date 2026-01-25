d9e5a2e12db5024f6063fbaadd55cad3c7bf4058
SwadebDeb sharma <swadebriva2012@gmail.com>
2026-01-26 02:02:37 +0600

    added payment intent and post information

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-
-app.get('/', (req, res)=>{
-      res.send('zap-shift server is running')
+app.get("/", (req, res) => {
+  res.send("zap-shift server is running");
 });
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fvmax46.mongodb.net/?appName=Cluster0`;
+const stripe = require('stripe')(process.env.PAYMENT_GATEWAY_KEY);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -32,55 +32,130 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const parcelCollection = client.db('zap-shift-DB').collection('parcels');
+    const parcelCollection = client.db("zap-shift-DB").collection("parcels");
+    const paymentsCollection = client.db("zap-shift-DB").collection("payments");
 
-    app.get('/parcels', async(req, res)=>{
+    app.get("/parcels", async (req, res) => {
       const result = await parcelCollection.find().toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.get('/parcels', async(req, res)=>{
-      const userEmail = req.query.email;
-      
-      const query = userEmail ? {created_by: userEmail}: {};
-      const options ={
-        sort:{ createdAt: -1}
-      }
+    app.get("/parcels", async (req, res) => {
+      const email = req.query.email;
+
+      const query = email ? { created_by: email } : {};
+      const options = {
+        sort: { createdAt: -1 },
+      };
       const result = await parcelCollection.find(query, options).toArray();
-      res.status(201).send(result)
-    })
-
+      res.status(201).send(result);
+    });
 
     // POST: post a parcels api
-    app.post('/parcels', async(req, res)=>{
+    app.post("/parcels", async (req, res) => {
       const parcel = req.body;
       const result = await parcelCollection.insertOne(parcel);
-      res.status(201).send(result)
-    })
+      res.status(201).send(result);
+    });
 
     // Delete: a Parcel
-    app.delete('/parcels/:id', async(req, res)=>{
+    app.delete("/parcels/:id", async (req, res) => {
       const id = req.params.id;
-      const query ={_id : new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await parcelCollection.deleteOne(query);
-      res.send(result)
+      res.send(result);
+    });
+// GET: payments
+app.get('/payments', async(req, res)=>{
+  try{
+    const userEmail = req.query.email;
+  const query = {email: userEmail};
+  const options ={ sort: {paid_at: -1}};
 
-    })
+  const payments = await paymentsCollection.find(query, options).toArray();
+  res.send(payments)
+  }catch(error){
+    console.error('Error fetching payment history:', error);
+    res.status(500).send({message: 'failed to get payments'})
+  }
+})
+
+
+// POST: Record payment and update parcel status
+app.post('/payments', async(req, res)=>{
+  try{
+    const {parcelId, email, amount, paymentMethod, transactionId} = req.body;
+
+    const updateResult = await parcelCollection.updateOne(
+      {_id: new ObjectId(parcelId)},
+      {
+        $set:{
+          payment_status: 'paid'
+        }
+      }
+    );
+
+    if(updateResult.modifiedCount === 0){
+      return res.status(400).send({message: 'Parcel not found or already paid'});
+    }
+
+    const paymentDoc ={
+      parcelId,
+      email,
+      amount,
+      paymentMethod,
+      transactionId,
+      paid_at_string: new Date().toISOString(),
+      paid_at: new Date()
+    };
+
+
+    const paymentResult = await paymentsCollection.insertOne(paymentDoc);
+    res.status(201).send({
+      message:'Payment recorded and parcel marked as paid',
+      insertedId: paymentResult.insertedId
+    });
+
 
     
+  }catch(error){
+    console.log('Error payment result', error)
+  }
+})
 
+
+
+   // Create a Payment Intent
+app.post('/create-payment-intent', async (req, res) => {
+  try {
+    const amountInCents = req.body.amountInCense;
+    
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amountInCents ,// Convert to cents
+      currency:'usd' ,
+      payment_method_types: ['card'],
+    });
+    
+
+    // Send the client secret to the client
+    res.json({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-//     await client.close();
+    //     await client.close();
   }
 }
 run().catch(console.dir);
 
-
-app.listen(port, ()=>{
-      console.log(`zap-shift server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`zap-shift server is running on port ${port}`);
+});
